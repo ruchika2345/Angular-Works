9571a7b25735f0d772b2371350002dd64d63a327
ruchika2345 <ruchikadas397@gmail.com>
2023-05-03 11:41:29 +0545

    crud project completed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'crud-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud-app');
+    expect(app.openAddEditEmpForm).toEqual('crud-app');
   });
 
   it('should render title', () => {
